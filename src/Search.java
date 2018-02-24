d7948ee9648a3fda4d25f1a50dd9c425e5559156
Kyaw Khant Nyar <kyawkn@kyaws-mbp.home>
2018-02-24 09:47:35 -0500

    first commit

---
+++ src/Search.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+public class Search {
+
+    /**
+     * Search constructor
+     */
+
+    public Search() {
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
